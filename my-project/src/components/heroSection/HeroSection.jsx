2cbda4a56a4e29799b21496645d98fbcfaf285c1
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-10-23 09:46:47 +0530

    HeroSection and Filter

---
+++ my-project/src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,10 @@
+
+function HeroSection() {
+  return (
+    <div>
+        <img src='https://media.istockphoto.com/id/1449490038/photo/online-shopping-and-e-commerce-technology-concept-shopper-using-computer-laptop-to-input.jpg?s=1024x1024&w=is&k=20&c=VH4xCCh4Fj6JCJsbkVuTJGK9xgwKJCePHEhGnZyyKGk=' alt="tyu"/>
+    </div>
+  )
+}
+
+export default HeroSection
--- my-project/src/pages/home/Home.jsx
+++ my-project/src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import { useContext } from "react";
 import Layout from "../../components/layout/Layout";
 import myContext from "../../context/data/myContext";
+import HeroSection from "../../components/heroSection/HeroSection";
+import Filter from "../../components/filter/Filter";
 
 function Home() {
   
   return (
     <Layout>
-      Home
+      <HeroSection/>
+      <Filter/>
+      
+      
 
     </Layout>
   );
