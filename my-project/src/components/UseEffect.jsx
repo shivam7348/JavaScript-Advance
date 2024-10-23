fd6f1cd6319b6cc20cdeeca8182210b05eb815bf
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-10-23 20:24:55 +0530

    useEffect does accept function

--- my-project/src/components/UseEffect.jsx
+++ my-project/src/components/UseEffect.jsx
@@ -1,10 +1,22 @@
-import React from 'react'                      
-                                                // The effect hook is a side Effect function component used to      manipulate or dom directly where we fetch our data
+// import { Button } from '@material-tailwind/react'
+// import React, { useEffect, useState } from 'react'                      
+//                                                 // The effect hook is a side Effect function component used to      manipulate or dom directly where we fetch our data
 
-function UseEffect() {
-  return (
-    <div>UseEffect</div>
-  )
-}
+// function UseEffect() {
+//     const [count, setCount] = useState(0)
+//     useEffect(() => {
+//         console.log("useEffect")
+//     },[])
 
-export default UseEffect
+//     function updateMe() {
+//         setCount(count  + 1)
+//     }
+
+//     return (
+//     <>
+//     <Button onClick={updateMe}>UpdateMe</Button>
+//     </>
+//   )
+// }
+
+// export default UseEffect
