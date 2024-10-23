2469c0219be4ea06bd92a0662f8df271cddf1bf5
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-10-23 17:32:16 +0530

    Creating navbar

--- DevkunusEcmmerce/src/components/navbar/Navbar.jsx
+++ DevkunusEcmmerce/src/components/navbar/Navbar.jsx
@@ -1,59 +1,56 @@
-import { Link } from "react-router-dom";
-import SearchBar from "../searchBar/SearchBar";
-
-
 const Navbar = () => {
-    // navList Data
-    const navList = (
-        <ul className="flex space-x-3 text-white font-medium text-md px-5 ">
-            {/* Home */}
+  return (
+    <>
+      <header className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+          <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Flowbite
+            </span>
+          </a>
+          <div className="flex items-center space-x-6 rtl:space-x-reverse">
+            <a href="tel:5541251234" className="text-sm text-gray-500 dark:text-white hover:underline">
+              (555) 412-1234
+            </a>
+            <a href="#" className="text-sm text-blue-600 dark:text-blue-500 hover:underline">
+              Login
+            </a>
+          </div>
+        </div>
+      </header>
+      <nav className="bg-gray-50 dark:bg-gray-700">
+        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+          <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
             <li>
-                <Link to={'/'}>Home</Link>
+              <a href="#" className="text-gray-900 dark:text-white hover:underline" aria-current="page">
+                Home
+              </a>
             </li>
-            {/* All Product */}
             <li>
-                <Link to={'/allproduct'}>All Product</Link>
+              <a href="#" className="text-gray-900 dark:text-white hover:underline">
+                Company
+              </a>
             </li>
-            {/* Signup */}
             <li>
-                <Link to={'/signup'}>Signup</Link>
+              <a href="#" className="text-gray-900 dark:text-white hover:underline">
+                Team
+              </a>
             </li>
-            {/* User */}
             <li>
-                <Link to={'/'}>Kamal</Link>
+              <a href="#" className="text-gray-900 dark:text-white hover:underline">
+                Features
+              </a>
             </li>
-            {/* Admin */}
-            {/* <li>
-            </li> */}
-            {/* logout */}
-            {/* <li>
-            </li> */}
-            {/* Cart */}
-            <li>
-                <Link to={'/cart'}>
-                </Link>
-            </li>
-        </ul>
-    )
-    return (
-        <nav className="bg-pink-600 sticky top-0">
-            {/* main  */}
-            <div className="lg:flex lg:justify-between items-center py-3 lg:px-3 ">
-                {/* left  */}
-                <div className="left py-3 lg:py-0">
-                    <Link to={'/'}>
-                    <h2 className=" font-bold text-white text-2xl text-center">E-Bharat</h2>
-                    </Link>
-                </div>
-                {/* right  */}
-                <div className="right flex justify-center mb-4 lg:mb-0">
-                    {navList}
-                </div>
-                {/* Search Bar  */}
-                <SearchBar />
-            </div>
-        </nav>
-    );
-}
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
